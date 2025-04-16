6fa216f755c94de9079f7998f3ae3b5d13518e47
rfuehricht <r.fuehricht@gmail.com>
2025-04-16 14:15:33 +0200

    [TASK] Remove wrong class comment in LoadDefaultValues

--- Classes/Component/LoadDefaultValues.php
+++ Classes/Component/LoadDefaultValues.php
@@ -6,29 +6,7 @@ use Psr\Http\Message\ResponseInterface;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
 /**
- * This PreProcessor adds the possibility to load default values.
- * Values fot the first step are loaded to $gp values of other steps are stored
- * to the session.
- *
- * Example configuration:
- *
- * <code>
- * preProcessors.1.class = LoadDefaultValues
- * preProcessors.1.config.1.contact_via.defaultValue = email
- * preProcessors.1.config.2.[field1].defaultValue = 0
- * preProcessors.1.config.2.[field2].defaultValue {
- *       data = date : U
- *       strftime = %A, %e. %B %Y
- * }
- * preProcessors.1.config.2.[field3].defaultValue < plugin.tx_exampleplugin
- * <code>
- *
- * may copy the TS to the default validator settings to avoid redundancy
- * Example:
- *
- * validators.1.config.fieldConf.[field].errorCheck.1.notDefaultValue
- * validators.1.config.fieldConf.[field].errorCheck.1.notDefaultValue.defaultValue < plugin.tx_formhandler.settings.preProcessors.1.config.1.[field].defaultValue
- *
+ * This component adds the possibility to load default values.
  */
 class LoadDefaultValues extends AbstractComponent
 {
